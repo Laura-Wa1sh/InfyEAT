cd466fe873d4f5191c9f134ed5bbd972e054c052
40203574 <lwalsh14@qub.ac.uk>
2019-07-02 11:59:54 +0530

    Adding a splash screen linking to a home screen and 2 seperate login screens for customer and vendor

---
+++ InfyEAT/app/src/main/java/com/example/infyeat/HomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.infyeat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button custButton;
+    private Button vendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Button for customer login page
+        custButton = (Button)findViewById(R.id.custButton);
+        custButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+
+        });
+
+        //Button for vendor login page
+        vendButton = (Button)findViewById(R.id.vendButton);
+        vendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openVendorLoginActivity();
+            }
+
+        });
+
+    }
+
+    public void openLoginActivity(){
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void openVendorLoginActivity(){
+        Intent loginIntent = new Intent(this, VendorLoginActivity.class);
+        startActivity(loginIntent);
+    }
+}
---
+++ InfyEAT/app/src/main/java/com/example/infyeat/VendorLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.infyeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VendorLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_login);
+    }
+}
